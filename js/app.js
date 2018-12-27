f82934869e2ecfe31b2f3dc49f5fb8a8bee2613c
Unknown <yousri.tabbakh@gmail.com>
2018-12-27 01:57:20 +0100

    [CORRECTION]

--- js/app.js
+++ js/app.js
@@ -4,4 +4,13 @@
     fade: true,
    });
    //Fin Carousel Avis Accueil
+
+   //Animation Block perso casting
+   $('.block-prota .titre-block, .container-perso .block-perso').each(function(i) {
+      var $li = $(this);
+      setTimeout(function() {
+          $li.addClass('anim');
+      }, i * 500); // delay 500 ms
+   });
+   //Fin Animation Block perso casting
 })(jQuery);
