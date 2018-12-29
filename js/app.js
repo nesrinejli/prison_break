edc7f71a63e6270069ed60c0632414cb65a9b76a
Unknown <yousri.tabbakh@gmail.com>
2018-12-29 03:04:14 +0100

    [HTML/CSS/JS/IMG] Correction page d'accueil

--- js/app.js
+++ js/app.js
@@ -5,6 +5,39 @@
    });
    //Fin Carousel Avis Accueil
 
+   //Animation page d'accueil
+   var y = $(this).scrollTop();
+   console.log(y)
+   if (y < 420) {
+      $('.block-intro h1, .block-synopsis').each(function(i) {
+         var $li = $(this);
+         setTimeout(function() {
+             $li.addClass('anim');
+         }, i * 500); // delay 500 ms
+      });
+   }
+   if (y > 200) {
+      setInterval(function() { $('.block-freres .block-one').addClass('anim') }, 0);
+      setInterval(function() { $('.block-intro h1, .block-synopsis').addClass('anim') }, 0);
+   }
+   if (y > 590) {
+      setInterval(function() { $('.block-freres .block-two').addClass('anim') }, 0);
+      setInterval(function() { $('.block-intro h1, .block-synopsis').addClass('anim') }, 0);
+   }
+   $(document).scroll(function() {
+      var y = $(this).scrollTop();
+      console.log(y)
+      if (y > 200) {
+         setInterval(function() { $('.block-freres .block-one').addClass('anim') }, 0);
+         setInterval(function() { $('.block-intro h1, .block-synopsis').addClass('anim') }, 0);
+      }
+      if (y > 590) {
+          setInterval(function() { $('.block-freres .block-two').addClass('anim') }, 0);
+          setInterval(function() { $('.block-intro h1, .block-synopsis').addClass('anim') }, 0);
+      }
+  });
+   //Fin Animation page d'accueil
+
    //Animation Block perso casting
    $('.block-prota .titre-block, .container-perso .block-perso').each(function(i) {
       var $li = $(this);
